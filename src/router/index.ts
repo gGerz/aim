48693cabb34d7d6e24b58050deab1a52b5034e11
gGerz70 <GZagrebin@sportmasterlab.net>
2025-04-18 15:20:44 +0700

    изменил роутер

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '@/store/authStore'
 import HomeView from '@/views/HomeView.vue'
 import AuthView from '@/views/AuthView.vue'
 import WorkspaceView from '@/views/WorkspaceView.vue'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
